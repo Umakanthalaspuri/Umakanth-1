498d2f8e4942ca24b19a379f0cf018940f9e086e
umakanth <umakanth.alasapuri15@gmail.com>
2025-07-16 18:40:26 +0530

    Pythone code test

---
+++ test1.py
@@ -0,0 +1,18 @@
+# Basic Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Define a variable
+name = "Umakanth"
+
+# Print the variable
+print("My name is", name)
+
+# Do some math
+a = 5
+b = 3
+sum = a + b
+
+# Print the result
+print("The sum of", a, "and", b, "is", sum)
